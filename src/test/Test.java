edcf2612ddc1da133b13b132920f911a8d37948c
Bartosz Mirowski <bartoszmirowski@yahoo.co.uk>
2016-02-08 11:44:40 +0000

    new commit

--- src/test/Test.java
+++ src/test/Test.java
@@ -27,7 +27,7 @@ public class Test extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: hello world ").append(request.getContextPath());
+		response.getWriter().append("Served at: Bartek ").append(request.getContextPath());
 	}
 
 	/**
