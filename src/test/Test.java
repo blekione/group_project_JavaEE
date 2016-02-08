4b3eafd4a88f0fc417d8f94ccd641aefd4bb467b
Marcin Kruglik <marcin.kruglik@gmail.com>
2016-02-08 11:42:29 +0000

    some play around

--- src/test/Test.java
+++ src/test/Test.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Test
  */
-@WebServlet("/Test")
+@WebServlet("/customer")
 public class Test extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,7 +27,7 @@ public class Test extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Served at: hello world ").append(request.getContextPath());
 	}
 
 	/**
